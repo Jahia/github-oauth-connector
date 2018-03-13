e41874dcee5f18eba7d86d0393b363f0a7f25dcf
Florent BOURASSE <fbourasse@jahia.com>
2018-03-13 09:06:59 +0100

    Remove of the license check

--- src/main/java/org/jahia/modules/githuboauthconnector/impl/GithubConnectorImpl.java
+++ src/main/java/org/jahia/modules/githuboauthconnector/impl/GithubConnectorImpl.java
@@ -3,19 +3,12 @@ package org.jahia.modules.githuboauthconnector.impl;
 import java.util.List;
 import java.util.Map;
 import org.jahia.modules.jahiaoauth.service.ConnectorService;
-import org.jahia.security.license.LicenseCheckException;
 
 public class GithubConnectorImpl implements ConnectorService {
 
     private String protectedResourceUrl;
     private String serviceName;
     private List<Map<String, Object>> availableProperties;
-
-    public void onStart() throws LicenseCheckException {
-//        if (!LicenseCheckerService.Stub.isAllowed("org.jahia.oauthGithub")) {
-//            throw new LicenseCheckException("Your DX license does not allow you to run Github OAuth Connector. please contact Jahia Solutions.");
-//        }
-    }
 
     @Override
     public String getProtectedResourceUrl() {
